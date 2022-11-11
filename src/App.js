3ca1fbafae653f09c4fd2497b6a627d13d50b36a
Thomas Kim <thomaskim@thomass-mbp-10.devices.brown.edu>
2022-11-10 19:09:11 -0500

    1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,18 +12,25 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [cartItems, setCartItems] = useState([])
+  const [cartPrices, setCartPrices] = useState(0)
 
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        <BakeryItem name={item.name} price={item.price} desc={item.description} 
+        img={item.image} cartItems={cartItems} setCartItems={setCartItems} cartPrices={cartPrices} setCartPrices={setCartPrices}/>
       ))}
 
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        <div>
+          {cartItems.map(item => <div>{item}</div>)} 
+        </div>
+        <div>Total: {cartPrices}</div>
+
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,19 @@
-// TODO: create a component that displays a single bakery item
+//TODO: create a component that displays a single bakery item
+import { useState } from "react";
+
+
+export default function BakeryItem({name, price, desc, img, cartItems, setCartItems, cartPrices, setCartPrices}) {
+    function handleClick() {  
+        setCartItems([...cartItems, name])
+        setCartPrices(cartPrices + price)
+    }
+
+    return (
+        <div>
+            <img src={img}/>
+            <h1>{name} {price} </h1>
+            <p>{desc}</p>
+            <button onClick={handleClick}> Add to Cart</button>
+        </div>
+    )    
+}
